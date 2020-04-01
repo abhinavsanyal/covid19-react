022ef8c22f02f8933c6cb25c73a26338b283e83c
Abhinava <reltronx@gmail.com>
2020-04-01 22:26:16 +0530

    Removed server

--- server/constants/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURL: "mongodb://eazma:password1@ds119533.mlab.com:19533/covid19",
-    jwtSecret: "mysecret"
-}
--- server/routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const { login, register } = require('./controllers/patientAuth/authController')
-
-router.post('/register', login)
-router.post('/authenticate', register)
-
-module.exports = router
