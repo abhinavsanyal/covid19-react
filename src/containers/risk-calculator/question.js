8aef23c94c924688485f14c229970729a836e77c
Sanjay <kumarsanjay435@gmail.com>
2020-04-03 12:02:23 +0530

    Risk-calculator-initialized

--- src/containers/risk-calculator/question.js
+++ src/containers/risk-calculator/question.js
@@ -1,11 +1,19 @@
 import React , { useState, useEffect} from 'react';
+import './index.scss';
+
 
 export default props => {
     
-     const {question,onAnswer} = props;
+     const {questn,onAnswer} = props;
+     const [isSelected, setSelected] = useState()
       return (
         <React.Fragment>
-         <div> this is the question component</div>
+        {questn && <div className="question-label">{questn.question}</div> }
+         <div className="answer-options">
+							<div className={"answer"} onClick={()=>{onAnswer(1)}}>YES</div>
+							<div className={"answer"} onClick={()=>{onAnswer(2)}}>No</div>
+							<div className={"answer"} onClick={()=>{onAnswer(3)}}>Maybe</div>
+						</div>
         </React.Fragment>
       );
 
